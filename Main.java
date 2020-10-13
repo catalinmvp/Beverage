b1d138938548b2f3dd2375162d67cde9a85daa9e
catalinmvp <nichitaandrei21@yahoo.com>
2020-10-13 13:53:41 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+package beverage;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Main implements Company1{
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    private static void test ()
+    {
+  /*      private List<Beverage> bevs1 =new ArrayList<Beverage>();
+        Beverage be2=new Beverage("BR","I1",1232,100,"01JAN","03FEB");
+        Beverage be3=new Beverage("BR1","I1",1235,98,"02JAN","05FEB");
+        Beverage be4=new Beverage("BR2","I2",1233,101,"04JAN","09FEB");
+        bevs1.add(b1);
+        bevs1.add(b2);
+        bevs1.add(b3);
+    */    
+        
+    }
+
+   
+}
